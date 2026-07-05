031009b2cdd948ad39f8bbe22f5f7c6d9e55c54b
matheusHen <matheushenriquesouzasantos2@gmail.com>
2026-07-05 14:04:13 -0300

    fix:ajustando datas para mostrar em formato brasileiro

--- src/ApiEstagioBicicletaria/Services/ClassesDeGeracaoDeRelatorios/RelatorioDeVendedoresComMaiorFaturamentoPorPeriodo.cs
+++ src/ApiEstagioBicicletaria/Services/ClassesDeGeracaoDeRelatorios/RelatorioDeVendedoresComMaiorFaturamentoPorPeriodo.cs
@@ -53,7 +53,9 @@ namespace ApiEstagioBicicletaria.Services.ClassesDeGeracaoDeRelatorios
                         table.Cell().ColumnSpan(5);
                         table.Cell().ColumnSpan(3).TranslateX(143).TranslateY(-45).AlignRight().AlignTop().PaddingBottom(-80).Width(120).Height(60).Image(caminhoImagem).FitArea();
                     });
-                    col.Item().Text($"Relatório de Vendedores Com Maior faturamento Por Período: {DataInicial} à {DataFinal}").FontSize(20).Bold();
+                    col.Item().Text($"Relatório de Vendedores Com Maior Faturamento Por Período: {DataInicial:dd/MM/yyyy} à {DataFinal:dd/MM/yyyy}")
+                    .FontSize(20)
+                    .Bold();
                     col.Item().PaddingVertical(10);
                     col.Item().Table(table =>
                     {
